974ed238fd01a68b9e03ff5ba9cb8e2b943aed4d
Flannon Jackson <flannon@nyu.edu>
2017-03-12 12:48:42 -0400

    Check to make sure btap.sh exists

--- configure.sh
+++ configure.sh
@@ -3,7 +3,9 @@
 set -e
 
 INSTALLDIR=/usr/local/bin
-BASENAME=btap.sh
+SCRIPT=btap.sh
+
+[[ -f $(cd "$(dirname "$0")" && pwd)/${SCRIPT} ]] || { echo "Not found ${SCRIPT}"; exit 3; }
 
 if  [[ $EUID -ne 0 ]]; then
   echo "$0 must be installed as root."
@@ -14,9 +16,9 @@ if [[ ! -f $INSTALLDIR ]]; then
   mkdir -p $INSTALLDIR
 fi
 
-if [[ ! -f "${INSTALLDIR}/${BASENAME}" ]]; then
-  cp -f ./${BASENAME} ${INSTALLDIR}/${BASENAME}
-  ln -s ${INSTALLDIR}/${BASENAME} ${INSTALLDIR}/${BASENAME::4}
+if [[ ! -f "${INSTALLDIR}/${SCRIPT}" ]]; then
+  cp -f ./${SCRIPT} ${INSTALLDIR}/${SCRIPT}
+  ln -s ${INSTALLDIR}/${SCRIPT} ${INSTALLDIR}/${SCRIPT::4}
 else
-  echo "${INSTALLDIR}/${BASENAME} exists"
+  echo "${INSTALLDIR}/${SCRIPT} exists"
 fi
